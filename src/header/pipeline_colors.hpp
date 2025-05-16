d3a667c6607da28ff720851e76e4fdb30406c256
Cago2000 <Cago2000@gmail.com>
2025-05-16 15:13:43 +0200

    pipeline returns color bounding boxes now

--- src/header/pipeline_colors.hpp
+++ src/header/pipeline_colors.hpp
@@ -2,7 +2,8 @@
 
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include "../header/bounding_box.hpp"
 
 namespace color_pipeline {
-    int start_pipeline_colors(std::vector<cv::Mat> color_images, std::vector<cv::Mat> resized_images);
+    std::vector<BoundingBox> start_pipeline_colors(std::vector<cv::Mat> color_images, std::vector<cv::Mat> resized_images);
 }
