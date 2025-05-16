88f8ef09dcbebb4f8b1b8decf1063c03add94fb8
Cago2000 <Cago2000@gmail.com>
2025-05-16 13:47:58 +0200

    fixed some typing and added to_string function

--- src/header/bounding_box.hpp
+++ src/header/bounding_box.hpp
@@ -10,28 +10,43 @@ class BoundingBox {
 public:
     int center_y;
     int center_x;
-    std::array<int, 4> box_corners;
+    std::vector<int> box_corners;
     int box_height;
     int box_width;
     int box_area;
-    std::array<int, 3> box_color;
+    cv::Vec3b box_color;
     int image_index;
 
-    BoundingBox(int y, int x, const std::array<int, 4>& corners, int height, int width, int area,
+    BoundingBox(int y, int x, std::vector<int> corners, int height, int width, int area,
                 cv::Vec3b box_color, int image_index);
 
-    std::string repr() const;
+    std::string to_string() const {
+        std::ostringstream oss;
+        oss << "BoundingBox(image_index=" << image_index
+            << ", center=(" << center_y << ", " << center_x << ")"
+            << ", corners=[top:" << box_corners[0]
+            << ", left:" << box_corners[1]
+            << ", bottom:" << box_corners[2]
+            << ", right:" << box_corners[3] << "]"
+            << ", height=" << box_height
+            << ", width=" << box_width
+            << ", area=" << box_area
+            << ", color=(B:" << static_cast<int>(box_color[0])
+            << ", G:" << static_cast<int>(box_color[1])
+            << ", R:" << static_cast<int>(box_color[2]) << ")"
+            << ")";
+        return oss.str();
+    }
 };
 
 namespace bounding_box {
-
-    std::vector<BoundingBox> create_bounding_boxes(const std::vector<std::vector<cv::Point>>& blobs, int image_index,
-                                                   int min_box_area, int max_box_area, cv::Vec3b box_color);
-
     BoundingBox create_bounding_box(const std::vector<cv::Point>& blob, int image_index,
                                                    int min_box_area, int max_box_area, cv::Vec3b& box_color);
 
-    cv::Mat draw_bounding_box(const BoundingBox& bounding_box, cv::Mat image);
+    std::vector<BoundingBox> create_bounding_boxes(const std::vector<std::vector<cv::Point>>& blobs, int image_index,
+                                                   int min_box_area, int max_box_area, cv::Vec3b& box_color);
+
+    cv::Mat draw_bounding_box(const BoundingBox& bounding_box, cv::Mat& image);
 
     std::vector<BoundingBox> fuse_bounding_box_matches(const std::vector<BoundingBox>& boxes1,
                                                        const std::vector<BoundingBox>& boxes2, int max_deviation);
